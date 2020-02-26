4c3dafc7fcaebbd5a625318943d82e63d796854f
stupidwolfy <61070268@kmitl.ac.th>
2020-02-26 14:36:13 +0700

    2

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\workkmitl\year2\term2\web pro\mini-proj\co-working\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
